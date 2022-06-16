62e190abf0c2d9fb10e3d36ad1bef47bd5efb1bb
ahmadsan <ahmadsan4p@gmail.com>
2022-06-16 20:39:38 +0800

    first commit

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+const nav = document.querySelector("#navBar")
+
+
+//ini merender menu navigasi
+const navMenu = ["Home", "Aboutme", "Social"]
+const navRender = () => {
+    for( let menu of navMenu) {
+        const li = document.createElement("li")
+        const a = document.createElement("a")
+        li.className = 'navmenu-button'
+        a.textContent = menu
+        a.href = `${menu}.html`
+        li.appendChild(a)
+        nav.appendChild(li)
+    }
+}
+
+navRender()
